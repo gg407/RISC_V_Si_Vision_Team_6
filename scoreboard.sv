9d1249040443411201a2786e4219d0d1f1dbfd1d
Mohamed Ahmed Hamed <104694689+mohamedyakoub@users.noreply.github.com>
2025-05-05 23:27:10 +0300

    Update scoreboard.sv

--- scoreboard.sv
+++ scoreboard.sv
@@ -122,26 +122,26 @@ task decode()
   endcase
 endtask
 task execute()
-  imm_e<=imm_d;
+  //imm_e<=imm_d;
   rf_write_addr<=rd_e;
   rf_write_en<=1;
   case(opcode)
-  7'b0010011:
+  I_TYPE_0:
              case(funct3)
-                2'h00: rf_write_data<= rs1_data+imm_e;
-                2'h01: rf_write_data<= rs1_data<<imm_e[4:0];
-                2'h02: rf_write_data<= (signed'(rs1_data)<signed'(imm_e))? 1:0;
-                2'h03: rf_write_data<= (rs1_data<imm_e)? 1:0;
-                2'h04: rf_write_data<= rs1_data^imm_e;
-                2'h05: 
-                      if( imm_e[11:5]==4'h00) rf_write_data<= rs1_data>>imm_e[4:0];
-                      else if(imm_e[11:5]==4'h20) rf_write_data<= rs1_data>>>imm_e[4:0];
-                2'h06: rf_write_data<= rs1_data|imm_e;
-                2'h07: rf_write_data<= rs1_data&imm_e;
+                addi: rf_write_data<= rs1_data+imm_d;
+                sllI: rf_write_data<= rs1_data<<imm_d[4:0];
+                sltI: rf_write_data<= (signed'(rs1_data)<signed'(imm_d))? 1:0;
+                sltIu: rf_write_data<= (rs1_data<imm_d)? 1:0;
+                xorI: rf_write_data<= rs1_data^imm_d;
+                srxI: 
+                      if( imm_e[11:5]==4'h00) rf_write_data<= rs1_data>>imm_d[4:0];
+                      else if(imm_e[11:5]==4'h20) rf_write_data<= rs1_data>>>imm_d[4:0];
+                orI: rf_write_data<= rs1_data|imm_d;
+                AndI: rf_write_data<= rs1_data&imm_d;
 
              endcase
-  7'b0110111: rf_write_data<= imm_e<<12;
-  7'b0010111: rf_write_data<= pc+(imm_e<<12);
+  U_TYPE_0: rf_write_data<= imm_e<<12;
+  U_TYPE_1: rf_write_data<= pc+(imm_e<<12);
 
   endcase
 endtask
